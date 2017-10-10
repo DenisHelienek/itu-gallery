adfa0192813d01c82c6a8daba0823db543a04e7b
Denis Helienek <denis.helienek@gmail.com>
2017-10-10 23:07:37 +0200

    font awesome added

--- gallery/resources/views/layouts/app.blade.php
+++ gallery/resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <div id="app">
@@ -29,7 +30,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        Web photogallery
+                        <i class="fa fa-camera"></i> Web photogallery-maker
                     </a>
                 </div>
 
