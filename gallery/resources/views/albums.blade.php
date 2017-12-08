48c1d07d78292cc0c74e8c5782fb387cec80da29
Jan Kotas <xkotas07@stud.fit.vutbr.cz>
2017-12-08 21:16:27 +0100

    Zmeny designu, oprava drobnych chyb, pridani podpory upravy popisku.

--- gallery/resources/views/albums.blade.php
+++ gallery/resources/views/albums.blade.php
@@ -1,77 +1,105 @@
 @extends('layouts.app')
 
 @section('content')
- <div class="row">
-  <div class="col-lg-12">
-    <div class="c_next"><a href="/theme"><p align="right">Next <i class="fa fa-arrow-right"></i></p></a></div>
-  </div>
-</div>
 
-<div class="row">
-  <div class="col-lg-2">
-    <div class="albums">
-      <ul>
-        <li><a href="#"><i class="fa fa-plus"></i> Add albums</a></li>
-        <div class="albums-list">
-          <li><a href="#">Kocicky</a></li>
-          <li><a href="#">Psici</a></li>
-          <li><a href="#">Medvede</a></li>
-          <li><a href="#">Kacicky</a></li>
-        </div>
-      </ul>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="t_next"><a href="/theme" style="text-decoration: none"><p align="right">Next <i class="fa fa-arrow-right"></i></p></a></div>
     </div>
   </div>
 
-  <div class="col-lg-10">
-    <div class="photos">
-      <ul>
-        <li><a href="#"><i class="fa fa-plus"></i> Add photos</a></li>
-      </ul>
-
-      <div class="row">
-        <div id="wrapper" class="photos-list">
-          <div class="col-lg-3">
-             <img src="{{ asset('img/cat.jpg') }}" class="img-thumbnail" >
-             <p>Cat</p>
+  <div class="row">
+    <div class="col-lg-2">
+      <div class="albums">
+        <ul>
+          <li><a href="#"><i class="fa fa-plus"></i> Add albums</a></li>
+          <div class="albums-list">
+            <li><a href="#">Kocicky</a></li>
+            <li><a href="#">Psici</a></li>
+            <li><a href="#">Medvede</a></li>
+            <li><a href="#">Kacicky</a></li>
           </div>
-           <div class="col-lg-3">
-             <img src="{{ asset('img/cat.jpg') }}" class="img-thumbnail" > 
-             <p>Cat</p> 
-          </div>       
-          <div class="col-lg-3">
-             <img src="{{ asset('img/cat.jpg') }}" class="img-thumbnail" > 
-             <p>Cat</p>
-          </div>
-          <div class="col-lg-3">
-             <img src="{{ asset('img/cat.jpg') }}" class="img-thumbnail" > 
-             <p>Cat</p>
-          </div>
-        </div>
+        </ul>
       </div>
-
-      <div class="row">
-        <div class="photos-list">
-          <div class="col-lg-3">
-             <img src="{{ asset('img/cat2.jpg') }}" class="img-thumbnail" > 
-             <p>Cat</p>
-          </div>
-           <div class="col-lg-3">
-             <img src="{{ asset('img/cat2.jpg') }}" class="img-thumbnail" > 
-             <p>Cat</p>
-          </div>       
-          <div class="col-lg-3">
-             <img src="{{ asset('img/cat2.jpg') }}" class="img-thumbnail" > 
-             <p>Cat</p>
-          </div>
-          <div class="col-lg-3">
-             <img src="{{ asset('img/cat2.jpg') }}" class="img-thumbnail" > 
-             <p>Cat</p>
-          </div>
-        </div>
-      </div>
-
-
     </div>
+
+    <div class="col-lg-10">
+      <div class="photos">
+        <ul>
+          <li><a href="#"><i class="fa fa-plus"></i> Add photos</a></li>
+        </ul>
+
+        <div class="row">
+          <div id="wrapper" class="photos-list">
+            <div class="col-lg-2">
+              <img src="{{ asset('img/cat.jpg') }}" class="img-thumbnail" >
+              <p id="0" class="changeable" contenteditable="true">Cat</p>
+            </div>
+             <div class="col-lg-2">
+               <img src="{{ asset('img/cat.jpg') }}" class="img-thumbnail" > 
+               <p id="1" class="changeable" contenteditable="true">Cat</p>
+            </div>       
+            <div class="col-lg-2">
+              <img src="{{ asset('img/cat.jpg') }}" class="img-thumbnail" > 
+              <p id="2" class="changeable" contenteditable="true">Cat</p>
+            </div>
+            <div class="col-lg-2">
+              <img src="{{ asset('img/cat.jpg') }}" class="img-thumbnail" > 
+              <p id="3" class="changeable" contenteditable="true">Cat</p>
+            </div>
+            <div class="col-lg-2">
+              <img src="{{ asset('img/cat.jpg') }}" class="img-thumbnail" > 
+              <p id="4" class="changeable" contenteditable="true">Cat</p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div id="wrapper" class="photos-list">
+            <div class="col-lg-2">
+              <img src="{{ asset('img/cat2.jpg') }}" class="img-thumbnail" >
+              <p id="5" class="changeable" contenteditable="true">Cat</p>
+            </div>
+             <div class="col-lg-2">
+              <img src="{{ asset('img/cat2.jpg') }}" class="img-thumbnail" > 
+              <p id="6" class="changeable" contenteditable="true">Cat</p>
+            </div>       
+            <div class="col-lg-2">
+              <img src="{{ asset('img/cat2.jpg') }}" class="img-thumbnail" > 
+              <p id="7" class="changeable" contenteditable="true">Cat</p>
+            </div>
+            <div class="col-lg-2">
+              <img src="{{ asset('img/cat2.jpg') }}" class="img-thumbnail" > 
+              <p id="8" class="changeable" contenteditable="true">Cat</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script type="text/javascript">
+      $(function(){
+      
+        $('body').on("click", ".changeable", function(e){
+          
+          var text = $(this).text();
+          
+          $(this).html("<input type='text' class='input-editable' value='" + text + "'>");
+          
+          $(this).find('input').focus();
+        });
+        
+        $('body').on("blur", ".input-editable", function(e){
+          
+          var text = $(this).val();
+        
+          $(this).parent().html(text);
+          
+          console.log("Value changes to: " + text);
+        });
+
+      });
+    </script>
   </div>
 </div> 
     
--- gallery/resources/views/theme.blade.php
+++ gallery/resources/views/theme.blade.php
@@ -1,68 +1,85 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="container-fluid">
  <div class="row">
-  <div class="col-lg-6">
-    <div class="t_back"><a href="{{ url('/gallery') }}"><p align="left"><i class="fa fa-arrow-left"></i> Back</p></a></div>
+    <div class="col-lg-6">
+      <div class="t_back"><a href="{{ url('/gallery') }}" style="text-decoration: none"><p align="left"><i class="fa fa-arrow-left"></i> Back</p></a></div>
+    </div>
+
+    <div class="col-lg-6">
+      <div class="t_next"><a href="{{ url('/final') }}" style="text-decoration: none"><p align="right">Next <i class="fa fa-arrow-right"></i></p></a></div>
+    </div>
   </div>
 
-  <div class="col-lg-6">
-    <div class="t_next"><a href="{{ url('/final') }}"><p align="right">Next <i class="fa fa-arrow-right"></i></p></a></div>
+  <div class="theme">
+    <div class="row">
+      <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+          <p>Number of images in one row</p>
+        </div>
+      <div class="col-lg-4"></div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+          <input id="s1" data-slider-id='slider' type="text" data-slider-min="0" data-slider-max="6" data-slider-step="1" data-slider-value="3"/>
+        </div>
+      <div class="col-lg-4"></div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+          <p>Number of rows in one page.</p>
+        </div>
+      <div class="col-lg-4"></div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+          <input id="s2" data-slider-id='slider' type="text" data-slider-min="0" data-slider-max="7" data-slider-step="1" data-slider-value="4"/>
+        </div>
+      <div class="col-lg-4"></div>
+    </div>
+
+    <div clas="row">
+      <p>Theme</p>
+    </div>
   </div>
-</div>
 
-<div class="theme">
-<div class="row">
-  <div class="col-lg-4"></div>
-    <div class="col-lg-4">
-      <p>Number of images in one row</p>
-    </div>
-  <div class="col-lg-4"></div>
-</div>
-
-<div class="row">
-  <div class="col-lg-4"></div>
-    <div class="col-lg-4">
-      <input id="s1" data-slider-id='slider' type="text" data-slider-min="0" data-slider-max="6" data-slider-step="1" data-slider-value="3"/>
-    </div>
-  <div class="col-lg-4"></div>
-</div>
-
-<div class="row">
-  <div class="col-lg-4"></div>
-    <div class="col-lg-4">
-      <p>Number of rows in one page.</p>
-    </div>
-  <div class="col-lg-4"></div>
-</div>
-
-<div class="row">
-  <div class="col-lg-4"></div>
-    <div class="col-lg-4">
-      <input id="s2" data-slider-id='slider' type="text" data-slider-min="0" data-slider-max="7" data-slider-step="1" data-slider-value="4"/>
-    </div>
-  <div class="col-lg-4"></div>
-</div>
-
-<div clas="row">
-  <p>Theme</p>
-</div>
-</div>
-
-
-
-<div class="row">
-  <div class="squares">
-    <a href="#"><div class="square light"></div></a>
   
-    <a href="#"><div class="square dark"></div></a>
-  
-    <a href="#"><div class="square aqua"></div></a> 
-
-    <a href="#"><div class="square artist"></div></a>  
+  <div class="container-fluid">
+    <div class="row">
+      <div class="squares">
+      <a href="#">
+        <div class="square-container">
+          <div class="square light"></div>
+          <p>Light</p>
+        </div>
+      </a>
+      <a href="#">
+        <div class="square-container">
+          <div class="square dark"></div>
+          <p>Dark</p>
+        </div>
+      </a>
+      <a href="#">
+        <div class="square-container">
+          <div class="square aqua"></div>
+          <p>Aqua</p>
+        </div>
+      </a> 
+      <a href="#">
+        <div class="square-container">
+          <div class="square artist"></div>
+          <p>Artist</p>
+        </div>
+      </a>  
+    </div>
   </div>
 </div>
-
 
 @endsection
