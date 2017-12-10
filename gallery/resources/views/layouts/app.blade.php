d90a66db1e3feb27f524fb4e108f0e2ce2725e1f
Denis Helienek <denis.helienek@gmail.com>
2017-12-10 17:09:09 +0100

    demo update

--- gallery/resources/views/layouts/app.blade.php
+++ gallery/resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.0/css/bootstrap-slider.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet">
 
     <!-- Scripts -->
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
--- gallery/resources/views/show.blade.php
+++ gallery/resources/views/show.blade.php
@@ -1,70 +1,93 @@
 @extends('layouts.app')
 
 @section('content')
+<?php
+  if(isset($_GET["name"])){
+    $newAlbum = $_GET["name"];
+    if(DB::select('select * from albums where name = :albName', ['albName' => $newAlbum]) == null) {
+      DB::insert('insert into albums (name, id_user, path) values (?, ?, ?)', [$newAlbum, \Auth::user()->id, "img/" . $newAlbum]);
+      // toto nefunguje - vytvorit album ve slozce img kam se budou ukladat obrazky alba
+      $result = File::makeDirectory(asset('img/') . $newAlbum);
+    }
+  }
+  $albums = DB::select('select * from albums where id_user = :id', ['id' => \Auth::user()->id]);
+  foreach ($albums as $album) {
+      //echo '<li><a href="#" style="cursor: pointer;">' . $album->name . '</a></li>';
+      break;
+  }
+?>
+
 <div class="container text-center">    
+<div class="theme-color">
 <div class="row">
-	<div class="col-lg-4"></div>
-	<div class="col-lg-4"><h1>Gallery demonstration</h1></div>
-	<div class="col-lg-4"></div>
+<div class="container gallery-container">
+
+    <h1>Gallery demonstration</h1>
+
+    <p class="page-description text-center">{{ $album->name}}</p>
+    
+    <div class="tz-gallery">
+
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="{{ asset('img/1.jpg')}}">
+                    <img src="{{ asset('img/1.jpg')}}" alt="Park">
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="{{ asset('img/2.jpg')}}">
+                    <img src="{{ asset('img/2.jpg')}}" alt="Park">
+                </a>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <a class="lightbox" href="{{ asset('img/3.jpg')}}">
+                    <img src="{{ asset('img/3.jpg')}}" alt="Park">
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="{{ asset('img/4.jpg')}}">
+                    <img src="{{ asset('img/4.jpg')}}" alt="Park">
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="{{ asset('img/5.jpeg')}}">
+                    <img src="{{ asset('img/5.jpeg')}}" alt="Park">
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="{{ asset('img/6.jpg')}}">
+                    <img src="{{ asset('img/6.jpg')}}" alt="Park">
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="{{ asset('img/7.jpeg')}}">
+                    <img src="{{ asset('img/7.jpeg')}}" alt="Park">
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="{{ asset('img/8.jpg')}}">
+                    <img src="{{ asset('img/8.jpg')}}" alt="Park">
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="{{ asset('img/9.jpg')}}">
+                    <img src="{{ asset('img/9.jpg')}}" alt="Park">
+                </a>
+            </div>
+        </div>
+
+    </div>
+
 </div>
 
-<div class="row">
-    <div class="col-lg-2">
-      <div class="albums">
-        <ul>
-          <li><h2>Album list</h2></li>
-          <div class="albums-list">
-            <?php
-
-              if(isset($_GET["name"])){
-                $newAlbum = $_GET["name"];
-        
-                if(DB::select('select * from albums where name = :albName', ['albName' => $newAlbum]) == null) {
-                  DB::insert('insert into albums (name, id_user, path) values (?, ?, ?)', [$newAlbum, \Auth::user()->id, "img/" . $newAlbum]);
-
-                  // toto nefunguje - vytvorit album ve slozce img kam se budou ukladat obrazky alba
-                  $result = File::makeDirectory(asset('img/') . $newAlbum);
-                }
-              }
-
-              $albums = DB::select('select * from albums where id_user = :id', ['id' => \Auth::user()->id]);
-              foreach ($albums as $album) {
-                  echo '<li><a href="#" style="cursor: pointer;">' . $album->name . '</a></li>';
-              }
-            ?>
-          </div>
-        </ul>
-      </div>
-    </div>
-
-    <div class="col-lg-10">
-      <div class="photos">
-        <ul>
-          <li><h1>{{ $album->name  }}</h1></li>
-        </ul>
-
-        <div class="show">
-	<div class="row">
-		<div class="col-lg-4"><img src="{{ asset('img/cat.jpg') }}" width="75%" style="border-radius: 25px;"></div>
-		<div class="col-lg-4"><img src="{{ asset('img/cat.jpg') }}" width="75%" style="border-radius: 25px;"></div>
-		<div class="col-lg-4"><img src="{{ asset('img/cat.jpg') }}" width="75%" style="border-radius: 25px;"></div>
-	</div>	
-
-	<div class="row">
-		<div class="col-lg-4"><img src="{{ asset('img/cat2.jpg') }}" width="75%" style="border-radius: 25px;"></div>
-		<div class="col-lg-4"><img src="{{ asset('img/cat2.jpg') }}" width="75%" style="border-radius: 25px;"></div>
-		<div class="col-lg-4"><img src="{{ asset('img/cat2.jpg') }}" width="75%" style="border-radius: 25px;"></div>
-	</div>	
-
-	<div class="row">
-		<div class="col-lg-4"><img src="{{ asset('img/cat.jpg') }}" width="75%" style="border-radius: 25px;"></div>
-		<div class="col-lg-4"><img src="{{ asset('img/cat.jpg') }}" width="75%" style="border-radius: 25px;"></div>
-		<div class="col-lg-4"><img src="{{ asset('img/cat.jpg') }}" width="75%" style="border-radius: 25px;"></div>
-	</div>		
-</div>   
+<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+<script>
+    baguetteBox.run('.tz-gallery');
+</script> 
 
       </div>
     </div>
+</div>
 </div>
 </div><br>
 
